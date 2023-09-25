ee0e67ad8bd5469fd6999647f1d574a8c6e609bd
BlackkTurtle <113180669+BlackkTurtle@users.noreply.github.com>
2023-09-25 21:47:41 +0300

    Initial commit

---
+++ src/test/java/org/planning/groovy/Lab4SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.planning.groovy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
